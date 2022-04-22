8b424a71ccb002bb45501091c621d38ec6b6aa3a
lukasmatusiewicz <lukas.matusiewicz@netknights.it>
2022-04-22 12:04:01 +0200

    Create TestValidateCheckWebauthn.php

---
+++ test/privacyidea-php-client/TestValidateCheckWebauthn.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once('../../src/Client-Autoloader.php');
+require_once('../../vendor/autoload.php');
+
+use PHPUnit\Framework\TestCase;
+use InterNations\Component\HttpMock\PHPUnit\HttpMockTrait;
+
+class TestValidateCheckWebauthn extends TestCase implements PILog
+{
+    private $pi;
+
+    use HttpMockTrait;
+
+    public static function setUpBeforeClass(): void
+    {
+        static::setUpHttpMockBeforeClass('8082', 'localhost');
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        static::tearDownHttpMockAfterClass();
+    }
+
+    public function setUp(): void
+    {
+        $this->setUpHttpMock();
+        $this->pi = new PrivacyIDEA('testUserAgent', "http://localhost:8082");
+    }
+
+    public function tearDown(): void
+    {
+        $this->tearDownHttpMock();
+    }
+
+    public function piDebug($message)
+    {
+        echo $message . "\n";
+    }
+
+    public function piError($message)
+    {
+        echo "error: " . $message . "\n";
+    }
+}
