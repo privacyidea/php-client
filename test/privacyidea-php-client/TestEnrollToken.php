846eb31f6796b3b054a70b537063e223bee9a81d
lukasmatusiewicz <lukas.matusiewicz@netknights.it>
2022-04-25 10:54:53 +0200

    Update TestEnrollToken.php

--- test/privacyidea-php-client/TestEnrollToken.php
+++ test/privacyidea-php-client/TestEnrollToken.php
@@ -6,7 +6,7 @@ require_once('../../vendor/autoload.php');
 use PHPUnit\Framework\TestCase;
 use InterNations\Component\HttpMock\PHPUnit\HttpMockTrait;
 
-class TestEnrollToken extends TestCase
+class TestEnrollToken extends TestCase implements PILog
 {
     private $pi;
 
@@ -25,7 +25,9 @@ class TestEnrollToken extends TestCase
     public function setUp(): void
     {
         $this->setUpHttpMock();
-        $this->pi = new PrivacyIDEA('testUserAgent', "http://127.0.0.1:8082");
+        $this->pi = new PrivacyIDEA('testUserAgent', "localhost:8082");
+        $this->pi->realm = "testRealm";
+        $this->pi->logger = $this;
     }
 
     public function tearDown(): void
@@ -36,173 +38,188 @@ class TestEnrollToken extends TestCase
     /**
      * @throws PIBadRequestException
      */
-    public function testEnrollToken()
+    public function testSuccess()
     {
-        // Test case if user already have a token
-        $respAuthToken = '{
-         "id": 1,
-         "jsonrpc": "2.0",
-         "result": {
-             "status": true,
-             "value": {
-                 "token": "eyJhbGciOiJIUz....jdpn9kIjuGRnGejmbFbM"
-             }
-         },
-         "version": "privacyIDEA unknown"
-        }';
+        $authToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NoBVmAurqcaaMmwM-AsD1S6chGIM";
 
-        $respTokenInfo = '{
-           "id":1,
-           "jsonrpc":"2.0",
-           "result":{
-              "status":true,
-              "value":{
-                 "count":3,
-                 "current":1,
-                 "tokens":[
-                    {
-                       "active":true,
-                       "count":37,
-                       "info":{
-                          "count_auth":"126",
-                          "tokenkind":"software"
-                       },
-                       "locked":false,
-                       "realms":[
-                          "testRealm"
-                       ],
-                       "resolver":"testResolver",
-                       "revoked":false
-                    }
-                 ]
-              }
-           },
-           "version":"privacyIDEA 3.5.2",
-           "versionnumber":"3.5.2",
-           "signature":"rsa_sha256_pss:12345"
-        }';
+        $img = "data:image/png;base64,iVBdgfgsdfgRK5CYII=";
 
-        $respTokenInit = '{
-           "detail":{
-              "googleurl":{
-                 "description":"URL for google Authenticator",
-                 "img":"data:image/png;base64,iVBORw0",
-                 "value":"otpauth://totp/TOTP0002A944?secret=Y5D5IM4H274ZI6NRO347QGQ4NPTIOHKL&period=30&digits=6&issuer=privacyIDEA"
-              },
-              "oathurl":{
-                 "description":"URL for OATH token",
-                 "img":"data:image/png;base64,iVBORw0",
-                 "value":"oathtoken:///addToken?name=TOTP0002A944&lockdown=true&key=c747d43387d7f99479b176f9f81a1c6be6871d4b&timeBased=true"
-              },
-              "otpkey":{
-                 "description":"OTP seed",
-                 "img":"data:image/png;base64,iVBORw0",
-                 "value":"seed://c747d43387d7f99479b176f9f81a1c6be6871d4b",
-                 "value_b32":"Y5D5IM4H274ZI6NRO347QGQ4NPTIOHKL"
-              },
-              "rollout_state":"",
-              "serial":"TOTP0002A944",
-              "threadid":140286414018304
-           },
-           "id":1,
-           "jsonrpc":"2.0",
-           "result":{
-              "status":true,
-              "value":true
-           },
-           "version":"privacyIDEA 3.5.2",
-           "versionnumber":"3.5.2",
-           "signature":"rsa_sha256_pss:12345"
-           }';
+        $responseBodyAuth = $this->authToken($authToken);
+
+        $responseBodyTokenInit = "{\n" . "    \"detail\": {\n" . "        \"googleurl\": {\n" .
+            "            \"description\": \"URL for google Authenticator\",\n" .
+            "            \"img\": \"data:image/png;base64,iVBdgfgsdfgRK5CYII=\",\n" .
+            "            \"value\": \"otpauth://hotp/OATH0003A0AA?secret=4DK5JEEQMWY3VES7EWB4M36TAW4YC2YH&counter=1&digits=6&issuer=privacyIDEA\"\n" .
+            "        },\n" . "        \"oathurl\": {\n" .
+            "            \"description\": \"URL for OATH token\",\n" .
+            "            \"img\": \"data:image/png;base64,iVBdgfgsdfgRK5CYII=\",\n" .
+            "            \"value\": \"oathtoken:///addToken?name=OATH0003A0AA&lockdown=true&key=e0d5d4909065b1ba925f2583c66fd305b9816b07\"\n" .
+            "        },\n" . "        \"otpkey\": {\n" .
+            "            \"description\": \"OTP seed\",\n" .
+            "            \"img\": \"data:image/png;base64,iVBdgfgsdfgRK5CYII=\",\n" .
+            "            \"value\": \"seed://e0d5d4909065b1ba925f2583c66fd305b9816b07\",\n" .
+            "            \"value_b32\": \"4DK5JEEQMWY3VES7EWB4M36TAW4YC2YH\"\n" .
+            "        },\n" . "        \"rollout_state\": \"\",\n" .
+            "        \"serial\": \"OATH0003A0AA\",\n" .
+            "        \"threadid\": 140470638720768\n" . "    },\n" .
+            "    \"id\": 1,\n" . "    \"jsonrpc\": \"2.0\",\n" .
+            "    \"result\": {\n" . "        \"status\": true,\n" .
+            "        \"value\": true\n" . "    },\n" .
+            "    \"time\": 1592834605.532012,\n" .
+            "    \"version\": \"privacyIDEA 3.3.3\",\n" .
+            "    \"versionnumber\": \"3.3.3\",\n" .
+            "    \"signature\": \"rsa_sha256_pss:\"\n" . "}";
 
         $this->http->mock
             ->when()
             ->methodIs('POST')
             ->pathIs('/auth')
             ->then()
-            ->body($respAuthToken)
-            ->end();
-        $this->http->setUp();
-
-        $this->http->mock
-            ->when()
-            ->methodIs('GET')
-            ->pathIs('/token/')
-            ->then()
-            ->body($respTokenInfo)
+            ->body($responseBodyAuth)
             ->end();
         $this->http->setUp();
 
         $this->http->mock
             ->when()
             ->methodIs('POST')
+            ->headerIs("Authorization", $authToken)
             ->pathIs('/token/init')
             ->then()
-            ->body($respTokenInit)
+            ->body($responseBodyTokenInit)
             ->end();
         $this->http->setUp();
+
+        $this->pi->serviceAccountName = "TestServiceAccount";
+        $this->pi->serviceAccountPass = "TestServicePass";
+        $this->pi->serviceAccountRealm = "TestServiceRealm";
 
         $response = $this->pi->enrollToken(
             "testUser",
             "1",
             "totp",
-            "Enrolled for Test");
-        $this->assertNotNull($response, "Response is NULL.");
-        $this->assertEmpty($response);
+            "Enrolled for test");
 
-        // Test case if user have no token and we should enroll a new one
-        $respTokenInfo = '{
-           "id":1,
-           "jsonrpc":"2.0",
-           "result":{
-              "status":true,
-              "value":{
-                 "count":3,
-                 "current":1,
-                 "tokens":[]
-              }
-           },
-           "version":"privacyIDEA 3.5.2",
-           "versionnumber":"3.5.2",
-           "signature":"rsa_sha256_pss:12345"
-        }';
+        $this->assertNotNull($response);
+        $this->assertIsObject($response);
+        $this->assertObjectHasAttribute('detail', $response);
+        $this->assertEquals($img, $response->detail->googleurl->img);
+    }
+
+    /**
+     * @throws PIBadRequestException
+     */
+    public function testNoServiceAccount()
+    {
+        $response = $this->pi->enrollToken(
+            "testUser",
+        "1",
+        "totp",
+        "Enrolled for test");
+
+        $this->assertNull($response);
+    }
+
+    /**
+     * @throws PIBadRequestException
+     */
+    public function testNoUsername()
+    {
+        $response = $this->pi->enrollToken(
+            "",
+            "1",
+            "totp",
+            "Enrolled for test");
+
+        $this->assertNull($response);
+    }
+
+    /**
+     * @throws PIBadRequestException
+     */
+    public function testUserAlreadyHasAToken()
+    {
+        $authToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NoBVmAurqcaaMmwM-AsD1S6chGIM";
+
+        $responseBodyAuth = $this->authToken($authToken);
+
+        $responseBodyGetToken = "{\"id\":1," . "\"jsonrpc\":\"2.0\"," . "\"result\":{" . "\"status\":true," . "\"value\":{" .
+            "\"count\":1," . "\"current\":1," . "\"tokens\":[{" . "\"active\":true," . "\"count\":2," .
+            "\"count_window\":10," . "\"description\":\"\"," . "\"failcount\":0," . "\"id\":347," .
+            "\"info\":{" . "\"count_auth\":\"1\"," . "\"count_auth_success\":\"1\"," .
+            "\"hashlib\":\"sha1\"," . "\"last_auth\":\"2022-03-2912:18:59.639421+02:00\"," .
+            "\"tokenkind\":\"software\"}," . "\"locked\":false," . "\"maxfail\":10," . "\"otplen\":6," .
+            "\"realms\":[\"defrealm\"]," . "\"resolver\":\"deflocal\"," . "\"revoked\":false," .
+            "\"rollout_state\":\"\"," . "\"serial\":\"OATH00123564\"," . "\"sync_window\":1000," .
+            "\"tokentype\":\"hotp\"," . "\"user_editable\":false," . "\"user_id\":\"5\"," .
+            "\"user_realm\":\"defrealm\"," . "\"username\":\"Test\"}]}}," . "\"time\":1648549489.57896," .
+            "\"version\":\"privacyIDEA3.6.3\"," . "\"versionnumber\":\"3.6.3\"," .
+            "\"signature\":\"rsa_sha256_pss:58c4eed1...5247c47e3e\"}";
+
+        $this->http->mock
+            ->when()
+            ->methodIs('POST')
+            ->pathIs('/auth')
+            ->then()
+            ->body($responseBodyAuth)
+            ->end();
+        $this->http->setUp();
 
         $this->http->mock
             ->when()
             ->methodIs('GET')
-            ->pathIs('/token/')
+            ->headerIs("Authorization", $authToken)
+            ->pathIs('/token')
             ->then()
-            ->body($respTokenInfo)
+            ->body($responseBodyGetToken)
             ->end();
         $this->http->setUp();
 
-        $response = $this->pi->enrollToken([
-            "user" => "",
-            "genkey" => "1",
-            "type" => "totp",
-            "description" => "Enrolled for Test"]);
-        $this->assertEmpty($response, "Without user given enrollToken() should return an empty array.");
+        $this->pi->serviceAccountName = "TestServiceAccount";
+        $this->pi->serviceAccountPass = "TestServicePass";
+        $this->pi->serviceAccountRealm = "TestServiceRealm";
 
-        $response = $this->pi->enrollToken([
-            "user" => "testUser",
-            "genkey" => "",
-            "type" => "totp"]);
-        $this->assertEmpty($response, "Without genkey given enrollToken() should return an empty array.");
+        $response = $this->pi->enrollToken(
+            "testUser",
+            "1",
+            "totp",
+            "Enrolled for test");
 
-        $response = $this->pi->enrollToken([
-            "user" => "testUser",
-            "genkey" => "1",
-            "type" => ""]);
-        $this->assertEmpty($response, "Without type given enrollToken() should return an empty array.");
+        $this->assertNull($response);
+    }
 
-        $response = $this->pi->enrollToken([
-            "user" => "testUser",
-            "genkey" => "1",
-            "type" => "totp",
-            "description" => "Enrolled for Test"]);
-        $this->assertNotNull($response, "Response is NULL.");
-        $this->assertIsObject($response);
-        $this->assertObjectHasAttribute('detail', $response, "Object have no detail attribute.");
-        $this->assertEquals("data:image/png;base64,iVBORw0", $response->detail->googleurl->img, "Object have no image data.");
+    /**
+     * @param string $authToken
+     * @return string
+     */
+    public function authToken($authToken)
+    {
+        return "{\n" . "    \"id\": 1,\n" . "    \"jsonrpc\": \"2.0\",\n" .
+            "    \"result\": {\n" . "        \"status\": true,\n" .
+            "        \"value\": {\n" . "            \"log_level\": 20,\n" .
+            "            \"menus\": [\n" . "                \"components\",\n" .
+            "                \"machines\"\n" . "            ],\n" .
+            "            \"realm\": \"\",\n" . "            \"rights\": [\n" .
+            "                \"policydelete\",\n" .
+            "                \"resync\"\n" . "            ],\n" .
+            "            \"role\": \"admin\",\n" . "            \"token\": \"" .
+            $authToken . "\",\n" . "            \"username\": \"admin\",\n" .
+            "            \"logout_time\": 120,\n" .
+            "            \"default_tokentype\": \"hotp\",\n" .
+            "            \"user_details\": false,\n" .
+            "            \"subscription_status\": 0\n" . "        }\n" .
+            "    },\n" . "    \"time\": 1589446794.8502703,\n" .
+            "    \"version\": \"privacyIDEA 3.2.1\",\n" .
+            "    \"versionnumber\": \"3.2.1\",\n" .
+            "    \"signature\": \"rsa_sha256_pss:\"\n" . "}";
+    }
+
+    public function piDebug($message)
+    {
+        echo $message . "\n";
+    }
+
+    public function piError($message)
+    {
+        echo "error: " . $message . "\n";
     }
 }
